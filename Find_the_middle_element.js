9e8c40bf87c31c64c69a70d242369e001500d1cc
h1ze <burgov@mail.ru>
2022-07-03 22:27:03 +0400

    Find_the_middle_element.js

---
+++ Find_the_middle_element.js
@@ -0,0 +1,8 @@
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+function gimme(triplet) {
+  let result = triplet.slice().sort((a, b) => a - b);
+  return triplet.indexOf(result[1]);
+}
